ed3a3b62cfe33605098095aa5b90d7b05fb39120
Noah <ngoodwin1999@gmail.com>
2022-06-24 07:22:49 -0400

    initial commit

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Address {
+
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
+
+}
---
+++ src/main/java/org/example/Phone.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Phone {
+
+    private String mob;
+
+}
